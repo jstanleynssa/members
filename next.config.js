456e31c26f44bfcb8c54b394469e18c5a33e630f
jstanleynssa <jstanley@nssapros.com>
2026-06-02 11:46:43 -0400

    Add webpack configuration for sharp external

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'sharp']
+    }
+    return config
+  },
 }
+
 module.exports = nextConfig
