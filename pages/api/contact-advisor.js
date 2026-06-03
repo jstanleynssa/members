08c793cab1f0c3de2be360309510bd00586859a7
jstanleynssa <jstanley@nssapros.com>
2026-06-03 09:21:34 -0400

    Update FROM address for email notifications

--- pages/api/contact-advisor.js
+++ pages/api/contact-advisor.js
@@ -6,7 +6,7 @@
 const RESEND_ENDPOINT = 'https://api.resend.com/emails'
 
 // From address must be on a Resend-verified domain (e.g. mail.nssapros.com).
-const FROM = 'NSSA Advisor Directory <directory@mail.nssapros.com>'
+const FROM = 'NSSA Advisor Directory <directory@updates.nssapros.com>'
 const SITE = 'https://directory.nssapros.com'
 
 function escapeHtml(str) {
