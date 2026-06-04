d83e1186b1d55e85a507b024bdf32de51b11ac5b
jstanleynssa <jstanley@nssapros.com>
2026-06-04 13:21:17 -0400

    Update API endpoint for profile saving

--- pages/profile/edit.js
+++ pages/profile/edit.js
@@ -161,7 +161,7 @@ export default function ProfileEdit({ member, userEmail }) {
     setSaveError(null)
 
     try {
-      const res = await fetch('/api/profile/save', {
+      const res = await fetch('/api/save', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
